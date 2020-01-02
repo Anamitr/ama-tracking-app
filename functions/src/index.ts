7ee518a091ee4f8e5191d4a5baaa71bf2acaea76
Konrad Sochacki <sochacki.konrad@gmail.com>
2020-01-02 14:16:17 +0100

    Add getNextLogId endpoint and convert date to Date format

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,7 @@ import * as functions from 'firebase-functions';
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.getConfig = require('./config-endpoints').getConfig
-exports.updateConfig = require('./config-endpoints').updateConfig
-exports.postLog = require('./logging-endpoints').postLog
+exports.getConfig = require('./config-endpoints').getConfig;
+exports.updateConfig = require('./config-endpoints').updateConfig;
+exports.postLog = require('./logging-endpoints').postLog;
+exports.getNextLogId = require('./logging-endpoints').getNextLogId;
--- functions/src/logging-endpoints.ts
+++ functions/src/logging-endpoints.ts
@@ -5,11 +5,16 @@ exports.postLog = functions.https.onRequest( (request, response) => {
     let configId = request.query.id;
     let data = request.body;
     let db = admin.firestore();
-    let logsRef = db.collection("logs").doc(configId).collection("logs");
-    logsRef.add(data).then((doc: any) => {
-        console.log("Added log with ID: ", doc.id, " - ", request.body);
-        response.status(201).send(data);
+    let logsRef = db.collection("logs").doc(configId).collection("logs").doc(data.id);
+
+    data.date = new Date(data.date);
+
+    logsRef.set(data).then((doc: any) => {
+        let responseMessage = ('Added log with ID: ${doc.id}' + doc.id + " - " + request.body);
+        console.log(responseMessage);
+        response.status(201).send(responseMessage);
     });
+
 });
 
 function getTestLog() {
@@ -19,3 +24,28 @@ function getTestLog() {
     };
     return testLog;
 }
+
+function getEmptyLog() {
+    let testLog = {
+        "date" : "",
+        "content" : "test log"
+    };
+    return testLog;
+}
+
+exports.getNextLogId = functions.https.onRequest(((request, response) => {
+    let configId = request.query.id;
+    let db = admin.firestore();
+    let logsRef = db.collection("logs").doc(configId).collection("logs").doc();
+    let nextLogId = logsRef.id
+    console.log("Returning next nextLogId: ", nextLogId)
+    response.send(nextLogId);
+
+    // logsRef.add(getEmptyLog()).then((doc: any) => {
+    //     console.log("Created empty log with id: ", doc.id);
+    //     response.send(doc.id);
+    // });
+
+    // console.log(logsRef.id);
+    // response.send(logsRef.doc().id);
+}));
